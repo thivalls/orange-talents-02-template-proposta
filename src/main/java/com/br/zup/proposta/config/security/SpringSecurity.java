3cb64e53e3cdccf48f0bfac30063b32cb46d7d2d
valls <olavalls@gmail.com>
2021-04-07 14:54:52 -0300

    Creating OAuth2 with keycloak spring security configuration

---
+++ src/main/java/com/br/zup/proposta/config/security/SpringSecurity.java
@@ -0,0 +1,25 @@
+package com.br.zup.proposta.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SpringSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_proposals:read")
+                        .antMatchers(HttpMethod.GET, "/cards/**").hasAuthority("SCOPE_cartoes:read")
+                        .antMatchers(HttpMethod.POST, "/cards/**").hasAuthority("SCOPE_cartoes:write")
+                        .antMatchers(HttpMethod.POST, "/proposals/**").hasAuthority("SCOPE_proposals:write")
+                        .anyRequest().authenticated()
+        )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+}
